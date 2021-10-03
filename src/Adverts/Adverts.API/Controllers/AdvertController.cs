a7990be10a8697deb1fb66ecfdb1ecd70d17be3e
Vural Vardarelli <vuralvardarelli@hotmail.com>
2021-10-03 22:27:56 +0300

    Add endpoints responseType's for swagger (Adverts.API)

--- src/Adverts/Adverts.API/Controllers/AdvertController.cs
+++ src/Adverts/Adverts.API/Controllers/AdvertController.cs
@@ -1,4 +1,5 @@
 ﻿using Adverts.API.Util;
+using Adverts.Core.Entities;
 using Adverts.Core.Models;
 using Adverts.Core.Models.Request;
 using Adverts.Infrastructure.Services.Interfaces;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -28,6 +30,9 @@ namespace Adverts.API.Controllers
         }
 
         [HttpGet("all")]
+        [ProducesResponseType(typeof(List<Core.Entities.Adverts>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         public async Task<ActionResult> GetAll()
         {
             GenericResult result = await _requestService.GetAll();
@@ -47,6 +52,9 @@ namespace Adverts.API.Controllers
         }
 
         [HttpGet("get")]
+        [ProducesResponseType(typeof(AdvertDetails), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         public async Task<ActionResult> GetById(string id)
         {
             GenericResult result = await _requestService.GetById(id);
@@ -66,19 +74,28 @@ namespace Adverts.API.Controllers
         }
 
         [HttpPost("visit")]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         public async Task<ActionResult> Visit([FromBody] AdvertVisitRequest request)
         {
-            AdvertVisitEvent eventMessage = new AdvertVisitEvent()
+            try
             {
-                advertId = Convert.ToInt32(request.advertId),
-                iPAdress = RequestInformation.GetIp(HttpContext),
-                RequestId = Guid.NewGuid(),
-                visitDate = DateTime.Now
-            };
+                AdvertVisitEvent eventMessage = new AdvertVisitEvent()
+                {
+                    advertId = Convert.ToInt32(request.advertId),
+                    iPAdress = RequestInformation.GetIp(HttpContext),
+                    RequestId = Guid.NewGuid(),
+                    visitDate = DateTime.Now
+                };
 
-            _eventBus.PublishAdvertVisit(EventBusConstants.AdvertVisitQueue, eventMessage);
+                _eventBus.PublishAdvertVisit(EventBusConstants.AdvertVisitQueue, eventMessage);
 
-            return Accepted();
+                return StatusCode(201, "visit created");
+            }
+            catch
+            {
+                return StatusCode(500, "Internal error occured");
+            }
         }
     }
 }
