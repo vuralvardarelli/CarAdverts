f3c927c3f6d22b415c302c20668f35342f2aaeee
Vural Vardarelli <vuralvardarelli@hotmail.com>
2021-10-03 11:31:23 +0300

    Add project files.

---
+++ src/Repository/Repository.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Repository.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Repository/Repository.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Repository.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
